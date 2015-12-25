270e874b57e19dd2eb38df8fa6604941316b2412
Niroj Pokhrel <nirojpokhrel@gmail.com>
2015-12-26 00:43:36 +0100

    "Session.sh added"

---
+++ Session.sh
@@ -0,0 +1,44 @@
+if [ "$1" = "Start" ]; 
+then
+	DIFF_FROM_ORIGIN=`git diff origin/master`
+#	DIFF_FROM_ORIGIN=`ls -l`
+	if [ ! $DIFF_FROM_ORIGIN ]; 
+	then
+		echo "Session Started Successfully"
+	else
+		echo "There is some commit in GIT.";
+		echo "$DIFF_FROM_ORIGIN";
+		read -p "Continue to Update?[Yy] " UPDATE
+		case "$UPDATE" in 
+   			 [Yy]*) 
+				git merge origin/master 
+				echo "Session Started Successfully"
+				;;
+        		 *)
+            			echo "SESSION DIDN'T START PROPERLY. 
+				      ARE YOU SURE TO CONTINUE WITHOUT SYNCING WITH THE ORIGIN?"
+				;;
+		esac
+	fi
+elif [ "$1" = "Stop" ];
+then
+	CHANGES_MADE=`git status`
+	if [ "$CHANGES_MADE" = "Nothing to Commit" ];
+	then
+		echo "No changes made. Session Stopped successfully"
+	else
+		#echo $CHANGES_MADE
+		DIFF_FROM_ORIGIN_STOP=`git diff origin/master`
+		if [ -z "$DIFF_FROM_ORIGIN_STOP" ];
+		then
+			echo "Origin has changed. Will merge before commiting."
+			git merge origin/master
+		fi
+		read -p "Commit Message?" MESSAGE_1
+		git add --all
+		git commit -m "$MESSAGE_1"
+		git push origin master
+		echo "Session Stopped successfully."
+	fi
+fi
+
